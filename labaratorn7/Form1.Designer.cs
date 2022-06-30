16e30b6b3027155397f5d6a0455ecfa01bb3e3cc
danhacker12 <82089973+danhacker12@users.noreply.github.com>
2022-06-29 21:45:45 -0700

    Add files via upload

---
+++ labaratorn7/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿
+namespace labaratorn7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar2 = new System.Windows.Forms.HScrollBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.x_label_value = new System.Windows.Forms.Label();
+            this.y_label_value = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Скорость";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(36, 196);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Ускорение";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(224, 113);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(136, 22);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Visible = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(224, 197);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(136, 22);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 266);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "а:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 345);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "b:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(134, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Закон движения";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(183, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "x=";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(183, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "y=";
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(168, 266);
+            this.hScrollBar1.Maximum = 1009;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(192, 17);
+            this.hScrollBar1.TabIndex = 14;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // hScrollBar2
+            // 
+            this.hScrollBar2.Location = new System.Drawing.Point(168, 345);
+            this.hScrollBar2.Maximum = 1009;
+            this.hScrollBar2.Name = "hScrollBar2";
+            this.hScrollBar2.Size = new System.Drawing.Size(192, 17);
+            this.hScrollBar2.TabIndex = 15;
+            this.hScrollBar2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar2_Scroll);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(205, 410);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 65);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Выход";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // x_label_value
+            // 
+            this.x_label_value.AutoSize = true;
+            this.x_label_value.Location = new System.Drawing.Point(205, 34);
+            this.x_label_value.Name = "x_label_value";
+            this.x_label_value.Size = new System.Drawing.Size(0, 17);
+            this.x_label_value.TabIndex = 17;
+            // 
+            // y_label_value
+            // 
+            this.y_label_value.AutoSize = true;
+            this.y_label_value.Location = new System.Drawing.Point(205, 51);
+            this.y_label_value.Name = "y_label_value";
+            this.y_label_value.Size = new System.Drawing.Size(0, 17);
+            this.y_label_value.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(205, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 17);
+            this.label6.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.OliveDrab;
+            this.ClientSize = new System.Drawing.Size(412, 487);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.y_label_value);
+            this.Controls.Add(this.x_label_value);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.hScrollBar2);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Кинематическая точка";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.HScrollBar hScrollBar2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label x_label_value;
+        private System.Windows.Forms.Label y_label_value;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ labaratorn7/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labaratorn7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+            this.textBox1.Text = "0";
+            this.textBox2.Text = "0";
+        }
+        
+        public const double PI = 3.1415926535897931;
+        public const double T = 5;
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Visible == true)
+            {
+                textBox1.Visible = false;
+            }
+            else
+            {
+                textBox1.Visible = true;
+            }
+            
+            
+
+        }
+        
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Visible == true)
+            {
+                textBox2.Visible = false;
+            }
+            else
+            {
+                textBox2.Visible = true;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)        {
+
+        }
+        public void MyMethod(double scrol_1, double scrol_2)
+        {
+            textBox1.Text = scrol_1.ToString();
+            double one_1 = Convert.ToDouble(textBox2.Text);//b
+            x_label_value.Text = Convert.ToString(PI * scrol_1  * Math.Cos(PI * one_1 * T));
+
+
+            textBox2.Text = scrol_2.ToString();
+            double two_2 = Convert.ToDouble(textBox1.Text);//a
+            y_label_value.Text = Convert.ToString(PI * scrol_1 * Math.Sin(PI * one_1 * T));
+        }
+        
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+            MyMethod(e.NewValue,Convert.ToDouble(textBox2.Text));
+            
+        }
+
+
+        private void hScrollBar2_Scroll(object sender, ScrollEventArgs e)
+        {
+            MyMethod(Convert.ToDouble(textBox1.Text),e.NewValue);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+          
+            Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+     
+       
+
+        
+
+        
+    }
+ 
+}
